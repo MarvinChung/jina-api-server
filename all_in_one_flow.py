b74a0b1a6434165cf54fe53bdd8db43acfd9b063
MarvinChung <marvin852316497@gmail.com>
2023-05-22 23:31:07 +0800

    add flow

--- all_in_one_flow.py
+++ all_in_one_flow.py
@@ -1,17 +1,46 @@
-from jina import DocumentArray, Executor, requests, dynamic_batching
+from jina import DocumentArray, Executor, requests, dynamic_batching, Flow
 from sentence_transformers import SentenceTransformer
-import torch
-import torch.nn.functional as F
-# from docarray import DocList, BaseDoc
-from docarray import DocumentArray
+from docarray import DocumentArray, Document
 from typing import Generator, Optional, Union, Dict, List, Any
+from pydantic import BaseModel, Field
 from jina.serve.runtimes.gateway.http.fastapi import FastAPIBaseGateway
-from jina import Document, DocumentArray, Flow, Executor, requests
 from fastapi import FastAPI
-import time
-import asyncio
-from executors import sBert
-from openai_api_protocol import *
+import torch
+
+class UsageInfo(BaseModel):
+    prompt_tokens: int = 0
+    total_tokens: int = 0
+    completion_tokens: Optional[int] = 0
+
+class EmbeddingsRequest(BaseModel):
+    model: Optional[str] = None
+    input: Union[str, List[Any]]
+    user: Optional[str] = None
+
+class EmbeddingsResponse(BaseModel):
+    object: str = "list"
+    data: List[Dict[str, Any]]
+    model: str
+    usage: UsageInfo
+
+
+class sBert(Executor):
+    """SBert embeds text into 384-dim vectors using all-MiniLM-L6-v2"""
+    def __init__(self, device: str = 'cpu', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.device = device
+        self.model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
+        self.model.to(device)  # Move the model to device
+
+    @requests(on='/embeddings')
+    @dynamic_batching(preferred_batch_size=4, timeout=200)
+    def embeddings(self, docs: DocumentArray, **kwargs):
+        
+        #"""Add text-based embeddings to all documents"""
+        with torch.inference_mode():
+            embeddings = self.model.encode(docs.texts)
+        docs.embeddings = embeddings
+
 
 class MyGateway(FastAPIBaseGateway):
     @property
@@ -20,8 +49,6 @@ class MyGateway(FastAPIBaseGateway):
 
         @app.post("/embeddings")
         async def create_embeddings(request: EmbeddingsRequest):
-            toc = time.time()
-
             token_num = 0
             "Creates embeddings for the text"""
             results = []
@@ -30,6 +57,7 @@ class MyGateway(FastAPIBaseGateway):
             if isinstance(request.input, str):
                 request.input = [request.input]
 
+            print(request.input)
             async for docs, error in self.streamer.stream(
                 docs=DocumentArray([Document(text=text_input) for text_input in request.input]),
                 exec_endpoint='/embeddings',
